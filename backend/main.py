084a512c65ea0707b374cae79df069a9a87f55c7
Ayano <s.correa.wilches@gmail.com>
2021-11-17 09:29:54 +0700

    Tutorial addon

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS
 
 
@@ -8,8 +8,18 @@ app = Flask(__name__)
 CORS(app)
 
 
+# 200 OK
+# 201 Created
+# 400 Bad Request
+# 500 Internal Server Error <- error suyo
+
 @app.route('/', methods=['GET'])
 def entry_point():
+    content = request.get_json(silent=True)
+    # revisar que por lo menos una pregunta sea verdadera
+    if content is None:
+        return Response("gonorrea", status=400, mimetype='application/json')
+
     return {'Hola': 'mundo'}
 
 
